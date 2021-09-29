5c71184e4590bb8351ac3fd79b0b8a4dea62632a
Josh Elrod <elrodjosh96@gmail.com>
2021-09-28 21:39:38 -0400

    begun time for loop

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 console.log("#currentDay");
+
+// moment statement to grab hour of the day
+// loop through text boxes, asking if hour matches hour of the day from moment function
+
+var currentHour = moment().format("H");
+console.log(currentHour);
+
+var hoursArray = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+
+var timeBlockArray = $(".list-group-item");
+console.log(timeBlockArray);
+
+function compareTime() {
+    for (var i = 0; i < hoursArray.length; i++);
+    console.log(compareTime);
+    if (hoursArray[i] === currentHour) {
+        $(".list-group-item").addClass('future');
+        console.log(compareTime);
+    } else hoursArray[i] < currentHour; 
+        $(".list-group-item").addClass('past');
+        console.log(hoursArray[i])
+}
+compareTime();
